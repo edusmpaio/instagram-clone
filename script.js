9bf7a74f125a0bfb61ce2eff5a4a880b00f72f00
Eduardo Sampaio <98369541+edusmpaio@users.noreply.github.com>
2022-08-25 18:44:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const inputEmail = document.querySelector(".email-input");
+const inputPassword = document.querySelector(".password-input");
+const logInButton = document.querySelector(".login-button");
+
+inputEmail.addEventListener("keyup", () => {
+  checkInputs();
+});
+
+inputPassword.addEventListener("keyup", () => {
+  checkInputs();
+});
+
+function checkInputs() {
+  if (inputEmail.value.length > 0 && inputPassword.value.length > 5) {
+    logInButton.removeAttribute("disabled");
+  } else {
+    logInButton.setAttribute("disabled", "disabled");
+  }
+}
